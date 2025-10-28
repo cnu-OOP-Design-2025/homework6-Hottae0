06f41c8d238d0b3b0f4c6266fb4bc9cebaa52828
하태영 <hot_t4e0@hataeyeong-ui-MacBookAir.local>
2025-10-28 15:18:17 +0900

    과제제출

--- bird.h
+++ bird.h
@@ -2,6 +2,7 @@
 
 #include <iostream>
 
+
 class Bird {
     std::string species; 
 public:
@@ -28,3 +29,65 @@ public:
         std::cout << species << " has died" << std::endl;
      }
 };
+
+
+using namespace std;
+
+class Duck : public Bird{
+    public :
+
+    Duck() : Bird("Duck"){
+        cry();
+    }
+
+    ~Duck(){
+        cry();
+    }
+    
+    void cry () override{
+        cout <<  "Quack!" << endl;
+    }
+};
+
+class Penguin : public Bird{
+    public : 
+
+    Penguin() : Bird("Penguin"){
+        string species = "Penguin";
+        cry();
+    }   
+
+    void cry() override{
+        cout << "Squawk!" << endl;
+    }
+
+    void fly() override{
+        cout << "This bird can't fly." << endl;
+    }
+
+    ~Penguin() {
+        cry();
+    }
+};
+
+class Eagle : public Bird{
+    public : 
+
+    Eagle() : Bird("Eagle"){
+        string species = "Eagle";
+        cry();
+    }   
+
+    void cry() override{
+        cout << "Screech!" << endl;
+    }
+
+    void swim() override{
+        cout << "This bird can't swim." << endl;
+    }
+
+    ~Eagle() {
+        cry();
+    }
+};
+
